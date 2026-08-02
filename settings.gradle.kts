30d52df833e7a02359af6413d60c7cf66ae31c5b
asdf13110791253 <60729322+asdf13110791253@users.noreply.github.com>
2026-08-03 04:14:12 +0800

    reset repo config

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -4,11 +4,9 @@ pluginManagement {
         mavenCentral()
         gradlePluginPortal()
     }
-    // ✅ 关键：在这里声明插件版本，避免 Gradle 9.x 报
-    // "plugin already on classpath with unknown version"
     plugins {
         id("com.android.application") version "8.5.2"
-        id("com.android.library")     version "8.5.2"
+        id("com.android.library") version "8.5.2"
         id("org.jetbrains.kotlin.android") version "1.9.24"
     }
 }
@@ -19,12 +17,8 @@ dependencyResolutionManagement {
         google()
         mavenCentral()
         maven { url = uri("https://jitpack.io") }
-        // 👇 新增清华公开OpenCV镜像，完全免费不需要登录
-        maven { url = uri("https://mirrors.tuna.tsinghua.edu.cn/opencv/maven2/") }
     }
 }
-
-
 
 rootProject.name = "LingMiaoV2"
 include(":app")
