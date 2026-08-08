abe7027c4eb0d5579064b6b51f0822f5117b02bf
asdf13110791253 <60729322+asdf13110791253@users.noreply.github.com>
2026-08-08 21:18:48 +0800

    Update FloatingService.kt

--- app/src/main/java/com/lingmiao/v2/service/FloatingService.kt
+++ app/src/main/java/com/lingmiao/v2/service/FloatingService.kt
@@ -5,6 +5,7 @@ import android.app.AlertDialog
 import android.app.Notification
 import android.app.NotificationChannel
 import android.app.NotificationManager
+import android.app.PendingIntent // 🔥 补上了这行
 import android.app.Service
 import android.content.Context
 import android.content.Intent
