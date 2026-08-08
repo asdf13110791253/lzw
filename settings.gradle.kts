3ca79739c39fdd156dbaee050a4f0f1070f994d9
asdf13110791253 <60729322+asdf13110791253@users.noreply.github.com>
2026-08-09 07:09:19 +0800

    Update settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -7,8 +7,9 @@ pluginManagement {
     plugins {
         id("com.android.application") version "8.5.2"
         id("com.android.library") version "8.5.2"
-        id("org.jetbrains.kotlin.android") version "1.9.22"
-        id("org.jetbrains.kotlin.kapt") version "1.9.22"
+        // 关键：这里改成 1.9.24，和你根目录 build.gradle 里的 kotlin_version 保持一致
+        id("org.jetbrains.kotlin.android") version "1.9.24"
+        id("org.jetbrains.kotlin.kapt") version "1.9.24"
     }
 }
 
@@ -17,6 +18,8 @@ dependencyResolutionManagement {
     repositories {
         google()
         mavenCentral()
+        // 加上阿里云镜像，在 Termux 里下载飞快
+        maven { url = uri("https://maven.aliyun.com/repository/public") }
         maven { url = uri("https://jitpack.io") }
     }
 }
